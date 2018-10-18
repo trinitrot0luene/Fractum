67250c56a3ebcf8595bf24232dbd2e50fad53733
Trinitrotoluene <pmichallet00@gmail.com>
2018-10-18 20:22:51 +0200

    fix docs path

--- releaseDocs.sh
+++ releaseDocs.sh
@@ -4,7 +4,7 @@ set -e
 export VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community"
 export VisualStudioVersion="15.0"
 
-docfx ./docs/docfx.json
+docfx ./docfx_project/docfx.json
 
 SOURCE_DIR=$PWD
 TEMP_REPO_DIR=$PWD/../my-project-gh-pages
